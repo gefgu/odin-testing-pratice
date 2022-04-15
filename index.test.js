f06e4b742b88ab6c1fb94cbf55e62494a6baf444
gefgu <gefgu@hotmail.com>
2022-04-15 19:53:03 -0300

    Add calculator tests

--- index.test.js
+++ index.test.js
@@ -28,4 +28,48 @@ test("Reversion with numbers and symbols", () => {
   expect(reverseString("a2c4")).toBe("4c2a");
 });
 
+// Calculator
 
+test("Simple add", () => {
+  expect(calculator.add(1, 3)).toBe(4);
+});
+
+test("Add with negative numbers", () => {
+  expect(calculator.add(1, -3)).toBe(-2);
+});
+
+test("Simple subtract", () => {
+  expect(calculator.subtract(19, 3)).toBe(16);
+});
+
+test("Subtract with negative numbers", () => {
+  expect(calculator.subtract(1, -3)).toBe(4);
+});
+
+test("Subtract with the first lower than the second", () => {
+  expect(calculator.subtract(1, 3)).toBe(-2);
+});
+
+test("Simple multiply", () => {
+  expect(calculator.multiply(2, 3)).toBe(6);
+});
+
+test("Multiply with 1 negative number", () => {
+  expect(calculator.multiply(2, -3)).toBe(-6);
+});
+
+test("Multiply with both negative numbers", () => {
+  expect(calculator.multiply(-5, -3)).toBe(15);
+});
+
+test("Simple Divide", () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test("Divide with 1 negative number", () => {
+  expect(calculator.divide(15, -3)).toBe(-5);
+});
+
+test("Divide with both negative numbers", () => {
+  expect(calculator.divide(-30, -10)).toBe(3);
+});
